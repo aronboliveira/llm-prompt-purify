7bd4bdad8a40979b2ba8898a12fc6648ac94a291
Aron-PC <aron.b.96@gmail.com>
2025-05-20 23:20:45 -0300

    start updates for modals in dark-mode

--- src/app/under-development/under-development.component.ts
+++ src/app/under-development/under-development.component.ts
@@ -5,7 +5,11 @@ import {
   Inject,
   PLATFORM_ID,
   afterNextRender,
+  ViewChild,
+  ElementRef,
 } from "@angular/core";
+import { AfterViewInit, OnDestroy } from "@angular/core";
+import { fromEvent, Subject, Subscription, takeUntil } from "rxjs";
 @Component({
   selector: "app-under-development",
   standalone: true,
@@ -13,7 +17,12 @@ import {
   templateUrl: "./under-development.component.html",
   styleUrl: "./under-development.component.scss",
 })
-export class UnderDevelopmentComponent {
+export class UnderDevelopmentComponent implements AfterViewInit, OnDestroy {
+  @ViewChild("header", { static: false }) toolbar!: ElementRef<HTMLElement>;
+  #scrollSubscription: Subscription | null = null;
+  #timeout: NodeJS.Timeout | number | null = null;
+  destroy$ = new Subject<void>();
+  public fixHeader = true;
   constructor(
     private _renderer: Renderer2,
     @Inject(PLATFORM_ID) private _platformId: Object
@@ -21,6 +30,49 @@ export class UnderDevelopmentComponent {
     afterNextRender(
       () => isPlatformBrowser(this._platformId) && this.#showDevelopmentAlert()
     );
+  }
+  ngAfterViewInit(): void {
+    if (typeof window === "undefined") return;
+    const _toolbar = document.getElementById("devAlert");
+    if (!_toolbar) return;
+    this.toolbar.nativeElement = _toolbar;
+    this.#timeout = setTimeout(() => {
+      this._renderer.setStyle(
+        this.toolbar.nativeElement,
+        "transform",
+        `translateY(-${
+          parseInt(getComputedStyle(this.toolbar.nativeElement).height) * 10
+        }px)`
+      );
+    }, 10_000);
+    this.#scrollSubscription = fromEvent(window, "scroll")
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.#timeout && clearTimeout(this.#timeout);
+        this._renderer.setStyle(
+          this.toolbar.nativeElement,
+          "transform",
+          "translateY()"
+        );
+        this.#timeout = setTimeout(() => {
+          this._renderer.setStyle(
+            this.toolbar.nativeElement,
+            "transform",
+            `translateY(-${
+              parseInt(getComputedStyle(this.toolbar.nativeElement).height) * 10
+            }px)`
+          );
+        }, 10_000);
+      });
+  }
+  ngOnDestroy(): void {
+    this.#scrollSubscription?.unsubscribe();
+    this.destroy$.next();
+    this.destroy$.complete();
+    this.#timeout = null;
+    this.fixHeader = true;
+    if (typeof window === "undefined") return;
+    window.removeEventListener("scroll", () => this.#domHandler());
   }
   #showDevelopmentAlert(): void {
     setTimeout(() => {
@@ -31,24 +83,6 @@ export class UnderDevelopmentComponent {
         "This page is still under early development!"
       );
       this._renderer.setAttribute(alertDiv, "id", "devAlert");
-      const s = {
-        color: "red",
-        width: "100vw",
-        position: "fixed",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        top: "0",
-        backgroundColor: "#fff",
-        zIndex: "999",
-        fontWeight: "bold",
-        fontSize: "1rem",
-        padding: "10px",
-        boxShadow: "0 2px 5px rgba(0, 0, 0, 0.2)",
-      };
-      Object.keys(s).forEach(prop => {
-        this._renderer.setStyle(alertDiv, prop, (s as any)[prop]);
-      });
       document.body.firstChild
         ? this._renderer.insertBefore(
             document.body,
@@ -56,6 +90,31 @@ export class UnderDevelopmentComponent {
             document.body.firstChild
           )
         : this._renderer.appendChild(document.body, alertDiv);
+      setTimeout(() => {
+        if (typeof window === "undefined") return;
+        const toolbar = document.getElementById("devAlert");
+        if (!toolbar) return;
+        const el =
+          this.toolbar?.nativeElement || document.getElementById("devAlert");
+        if (!el) return;
+        this._renderer.setStyle(el, "transform", `translateY(0)`);
+        window.addEventListener("scroll", () => this.#domHandler(toolbar));
+      }, 200);
     }, 200);
+  }
+  #domHandler(toolbar?: HTMLElement): void {
+    if (!toolbar) return;
+    if (this.#timeout) clearTimeout(this.#timeout);
+    toolbar.style.transform = "translateY(0)";
+    this.#timeout = setTimeout(() => {
+      const el =
+        this.toolbar?.nativeElement || document.getElementById("devAlert");
+      if (!el) return;
+      this._renderer.setStyle(
+        el,
+        "transform",
+        `translateY(-${parseInt(getComputedStyle(el).height) * 10}px)`
+      );
+    }, 10000);
   }
 }
