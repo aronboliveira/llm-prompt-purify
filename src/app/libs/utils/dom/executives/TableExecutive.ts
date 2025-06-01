16ce84720aa6054175bbae7198523151c9fff22f
Aron-PC <aron.b.96@gmail.com>
2025-06-01 02:33:13 -0300

    include sorting algorithms

--- src/app/libs/utils/dom/executives/TableExecutive.ts
+++ src/app/libs/utils/dom/executives/TableExecutive.ts
@@ -55,41 +55,71 @@ export default class TableExecutive {
       );
     });
   }
-  sortColumn(ev: MouseEvent | PointerEvent): void {
-    const queryForRelCell = (): HTMLElement | null => {
-      if (!(ev.currentTarget instanceof HTMLElement)) return null;
-      return (
-        ev.currentTarget.closest("th") ||
-        ev.currentTarget.closest("td") ||
-        ev.currentTarget.closest(`${appState.classes.matTh}`)
-      );
-    };
-    if (
-      !(
-        ev.button === 0 &&
-        ev.currentTarget instanceof HTMLElement &&
-        ev.currentTarget.isConnected &&
-        queryForRelCell()
+  sortColumn(ev: MouseEvent | PointerEvent): {
+    all: Element[];
+    targets: Element[];
+    prevOrders: Record<string, string>;
+  } | null {
+    try {
+      const queryForRelCell = (): HTMLElement | null => {
+        if (!(ev.currentTarget instanceof HTMLElement)) return null;
+        return (
+          ev.currentTarget.closest("th") ??
+          ev.currentTarget.closest("td") ??
+          ev.currentTarget.closest(`.${appState.classes.matTh}`)
+        );
+      };
+      if (
+        !(
+          ev.button === 0 &&
+          ev.currentTarget instanceof HTMLElement &&
+          ev.currentTarget.isConnected &&
+          queryForRelCell()
+        )
       )
-    )
-      return;
-    const cell = queryForRelCell();
-    if (!this.table?.isConnected)
-      this.setTable(document.getElementById(appState.ids.scanTab));
-    const firstRow = this.table.querySelector("tr");
-    if (!this.table || !firstRow || !cell) return;
-    new Set([
-      ...Array.from(firstRow.getElementsByTagName("th")),
-      ...Array.from(firstRow.getElementsByTagName("td")),
-      ...Array.from(firstRow.getElementsByClassName(appState.classes.matTh)),
-    ]).forEach(h => {
-      h.setAttribute(appState.patterns.activeSorting, "false");
-      h !== cell && h.setAttribute(appState.patterns.order, "asc");
-    });
-    cell.setAttribute(appState.patterns.activeSorting, "true");
-    this.#recollectAndSort(cell);
+        return null;
+      const cell = queryForRelCell();
+      if (!this.table?.isConnected)
+        this.setTable(document.getElementById(appState.ids.scanTab));
+      const firstRow = this.table?.querySelector("tr");
+      if (!this.table || !firstRow || !cell) return null;
+      const prevOrders: Record<string, string> = {};
+      let idxAcc = 0;
+      const headerCells = new Set<HTMLElement>([
+        ...Array.from(firstRow.getElementsByTagName("th")),
+        ...Array.from(firstRow.getElementsByTagName("td")),
+        ...(Array.from(
+          firstRow.getElementsByClassName(appState.classes.matTh)
+        ) as any as HTMLElement[]),
+      ]);
+      const prevTargOrder = cell.getAttribute(appState.patterns.order),
+        prevActive = cell.getAttribute(appState.patterns.activeSorting);
+      headerCells.forEach(h => {
+        idxAcc += 1;
+        h.setAttribute(appState.patterns.activeSorting, "false");
+        h.setAttribute(appState.patterns.order, "asc");
+        prevOrders[idxAcc.toString()] = "asc";
+      });
+      cell.setAttribute(appState.patterns.activeSorting, "true");
+      prevTargOrder &&
+        cell.setAttribute(
+          appState.patterns.order,
+          prevTargOrder === "asc" && prevActive === "true" ? "desc" : "asc"
+        );
+      const res = this.#getTargets(cell);
+      console.log(res);
+      return res ? { ...res, prevOrders } : null;
+    } catch (e) {
+      console.error(
+        `Error sorting table: ${(e as Error).name} — ${(e as Error).message}`
+      );
+      return null;
+    }
   }
-  #recollectAndSort(targCell: HTMLElement) {
+
+  #getTargets(
+    targCell: HTMLElement
+  ): { all: Element[]; targets: Element[] } | null {
     const pt = appState.patterns;
     let targCellIdx = targCell.getAttribute(pt.col);
     if (!targCellIdx) {
@@ -97,132 +127,64 @@ export default class TableExecutive {
         targCell.closest("tr") ??
         targCell.closest(`.${appState.classes.matTr}`) ??
         targCell.closest(`.${appState.classes.matTrAlt}`);
-      if (!targTr) return;
-      const setCells = Array.from(
-        new Set([
+      if (!targTr) return null;
+      const headerSet = Array.from(
+        new Set<HTMLElement>([
           ...Array.from(targTr.getElementsByTagName("th")),
-          ...Array.from(targTr.getElementsByClassName(appState.classes.matTh)),
+          ...(Array.from(
+            targTr.getElementsByClassName(appState.classes.matTh)
+          ) as any as HTMLElement[]),
         ])
       );
-      for (let i = 0; i <= setCells.length; i++) {
-        const c = setCells[i];
-        if (!(c instanceof HTMLElement)) return;
-        if (c === targCell) {
-          targCellIdx = (i + 1).toString();
-          break;
-        }
+      for (let i = 0; i < headerSet.length; i++) {
+        if (headerSet[i] === targCell) targCellIdx = (i + 1).toString();
+        break;
       }
+      if (!targCellIdx) return null;
     }
-    // rows =
-    //   this.#table instanceof HTMLTableElement
-    //     ? Array.from(this.#table.rows)
-    //     : Array.from(
-    //         new Set([
-    //           ...Array.from(this.#table.getElementsByTagName("tr")),
-    //           ...Array.from(
-    //             this.#table.querySelectorAll("[class*=mat-mdc-table-row]")
-    //           ),
-    //         ])
-    //       ),
-    // sortingHeader = Array.from(
-    //   new Set([
-    //     ...Array.from(rows[0].querySelectorAll("th")),
-    //     ...Array.from(rows[0].querySelectorAll(".mat-mdc-header-cell")),
-    //   ])
-    // )
-    //   .map<[number, Element]>((h, i) => [i, h])
-    //   .find(
-    //     ([_, h]) =>
-    //       h instanceof HTMLElement &&
-    //       h.getAttribute(pt.sort) &&
-    //       h.getAttribute(pt.order) &&
-    //       h.getAttribute(pt.activeSorting) === "true"
-    //   );
-    targCell.setAttribute(
-      pt.order,
-      targCell.getAttribute(pt.order) === "asc" ? "desc" : "asc"
-    );
-    let cells = Array.from(
+    const allCells: Element[] = Array.from(
       new Set(
         [
-          ...Array.from(this.#table.getElementsByTagName("td")),
-          ...Array.from(
-            this.#table.getElementsByClassName(appState.classes.matTd)
-          ),
+          ...Array.from(this.table.getElementsByTagName("td")),
+          ...(Array.from(
+            this.table.getElementsByClassName(appState.classes.matTd)
+          ) as any as Element[]),
         ].filter(
-          c =>
-            c instanceof HTMLElement &&
-            c.getAttribute(pt.col) === targCellIdx &&
-            !c.classList.contains(appState.classes.matTh)
+          c => c instanceof HTMLElement && !c.getAttribute(pt.activeSorting)
         )
       )
-    ).sort((a, b) => {
-      const criteria = targCell.getAttribute(pt.sort) || "index",
-        direction = targCell.getAttribute(pt.order) || "asc";
-      let result = 0;
-      if (criteria.toLowerCase().includes("pattern"))
-        result = (a.textContent || (a as HTMLElement).innerText || "")
-          .trim()
-          .localeCompare(
-            (b.textContent || (b as HTMLElement).innerText || "").trim(),
-            ["pt", "de", "es", "hb", "in", "ja", "ko", "zh", "ru", "fr", "it"],
-            {
-              sensitivity: "base",
-              numeric: false,
-              caseFirst: "false",
-              ignorePunctuation: false,
-            }
-          );
-      else if (criteria.toLowerCase().includes("suggested")) {
-        const getUnicodeValue = (str: string) => {
-            return Array.from(str).map(char => char.codePointAt(0));
-          },
-          unicodeA = getUnicodeValue(
-            (a.textContent || (a as HTMLElement).innerText || "").trim()
-          ),
-          unicodeB = getUnicodeValue(
-            (b.textContent || (b as HTMLElement).innerText || "").trim()
-          );
-        for (let i = 0; i < Math.min(unicodeA.length, unicodeB.length); i++) {
-          if (unicodeA[i] !== unicodeB[i]) {
-            result = (unicodeA?.[i] || 0) - (unicodeB?.[i] || 0);
-            break;
-          }
-        }
-        if (result === 0) result = unicodeA.length - unicodeB.length;
-      } else if (criteria.toLowerCase().includes("use_mask")) {
-        const checkboxA = a.querySelector('input[type="checkbox"]'),
-          checkboxB = b.querySelector('input[type="checkbox"]'),
-          checkedA = checkboxA
-            ? (checkboxA as HTMLInputElement).checked
-            : false,
-          checkedB = checkboxB
-            ? (checkboxB as HTMLInputElement).checked
-            : false;
-        if (checkedA === checkedB)
-          result = (a.textContent || (a as HTMLElement).innerText || "")
-            .trim()
-            .localeCompare(b.textContent || (b as HTMLElement).innerText || "");
-        else result = Number(checkedB) - Number(checkedA);
-      } else {
-        const getNumericValue = (element: HTMLElement) => {
-          const match = (element.textContent || element.innerText || "")
-              .trim()
-              .match(/-?\d+(\.\d+)?/),
-            parsed = match ? parseFloat(match[0]) : 0;
-          return !Number.isFinite(parsed) ? parsed : 0;
-        };
-        result =
-          getNumericValue(a as HTMLElement) - getNumericValue(b as HTMLElement);
-      }
-      return direction.toLowerCase() === "desc" ? -result : result;
-    });
-    console.log(targCell.getAttribute("data-orderby"));
-    if (targCell.getAttribute(pt.order) === "desc") {
-      cells = cells.reverse();
-      console.log(cells[0]);
-    } else {
-      console.log(cells[0]);
+    );
+    const targCells: Element[] = [];
+    for (const c of allCells) {
+      if (!(c instanceof HTMLElement)) continue;
+      const thisCol = c.getAttribute(pt.col);
+      if (!thisCol) continue;
+      const refHdId = c.getAttribute(pt.refHead),
+        fallbackRelTh = (): HTMLElement | null => {
+          return Array.from(
+            this.table.getElementsByClassName(appState.classes.matTh)
+          ).find(
+            h => (h as HTMLElement).getAttribute(pt.col) === thisCol
+          ) as HTMLElement | null;
+        },
+        linkedHeader: HTMLElement | null | undefined = refHdId
+          ? (() => {
+              const thRef = document.getElementById(refHdId);
+              return thRef && thRef.id.endsWith(thisCol)
+                ? thRef
+                : fallbackRelTh();
+            })()
+          : fallbackRelTh();
+      console.log("This col ", thisCol);
+      console.log(linkedHeader);
+      console.log(
+        "Active sorting ",
+        linkedHeader?.getAttribute(pt.activeSorting) || "NULL"
+      );
+      linkedHeader instanceof HTMLElement &&
+        linkedHeader.getAttribute(pt.activeSorting) === "true" &&
+        targCells.push(c);
     }
+    return { all: allCells, targets: targCells };
   }
 }
