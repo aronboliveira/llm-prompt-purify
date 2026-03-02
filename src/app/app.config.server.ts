4555f8ca256c7a0ab05c182bf0d60e763266e660
aronboliveira <aron.b.96@gmail.com>
2026-03-02 20:35:14 -0300

    chore: remove legacy prototype surfaces

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,8 +1,9 @@
 import { mergeApplicationConfig, ApplicationConfig } from "@angular/core";
 import { provideServerRendering } from "@angular/platform-server";
 import { appConfig } from "./app.config";
-import { provideClientHydration } from "@angular/platform-browser";
+
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(), provideClientHydration()],
+  providers: [provideServerRendering()],
 };
+
 export const config = mergeApplicationConfig(appConfig, serverConfig);
--- src/definitions/foundation.d.ts
+++
@@ -1,7 +0,0 @@
-export type imageLikeElement = HTMLInputElement | HTMLImageElement;
-export type inputLikeElement = HTMLInputElement | HTMLTextAreaElement;
-export type listElement = HTMLUListElement | HTMLOListElement | HTMLMenuElement;
-export type genericElement = HTMLDivElement | HTMLSpanElement;
-export type entryElement = inputLikeElement | HTMLSelectElement;
-export type disableableElement = entryElement | HTMLButtonElement;
-export type pressableElement = HTMLButtonElement | HTMLInputElement;
