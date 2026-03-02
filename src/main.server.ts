2dca94d0ef14f2cb545550666eaa2cc6c280d37d
aronboliveira <aron.b.96@gmail.com>
2026-03-02 20:35:14 -0300

    chore: remove legacy prototype surfaces

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,20 +1,7 @@
-import { BrowserModule, bootstrapApplication } from "@angular/platform-browser";
+import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { config } from "./app/app.config.server";
-import { importProvidersFrom } from "@angular/core";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatDialogModule } from "@angular/material/dialog";
-import { A11yModule } from "@angular/cdk/a11y";
-const bootstrap = () =>
-  bootstrapApplication(AppComponent, {
-    ...config,
-    providers: [
-      importProvidersFrom(
-        BrowserModule,
-        BrowserAnimationsModule,
-        MatDialogModule,
-        A11yModule
-      ),
-    ],
-  });
+
+const bootstrap = () => bootstrapApplication(AppComponent, config);
+
 export default bootstrap;
